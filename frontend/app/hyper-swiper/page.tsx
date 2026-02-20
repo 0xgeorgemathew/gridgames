eda757b650d3e76cc4f259d4bd9364afff40383b
George Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2026-02-20 20:40:30 +0530

    feat(hyper-swiper): add dedicated route for Hyper Swiper game
    - Add /hyper-swiper route with full game UI
    - Restore original game experience at dedicated path

---
+++ frontend/app/hyper-swiper/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useTradingStore } from '@/games/hyper-swiper/game/stores/trading-store'
+import { MatchmakingScreen } from '@/games/hyper-swiper/components/MatchmakingScreen'
+import { GameHUD } from '@/games/hyper-swiper/components/GameHUD'
+import { PositionIndicator } from '@/games/hyper-swiper/components/PositionIndicator'
+import { GameCanvasBackground } from '@/components/GameCanvasBackground'
+import { ToastNotifications } from '@/components/ToastNotifications'
+import { GameOverModal } from '@/games/hyper-swiper/components/GameOverModal'
+import { SettlementFlash } from '@/games/hyper-swiper/components/SettlementFlash'
+import { RoundEndFlash } from '@/games/hyper-swiper/components/RoundEndFlash'
+import GameCanvas from '@/components/GameCanvas'
+import { useEffect } from 'react'
+import { sdk } from '@farcaster/miniapp-sdk'
+
+export default function HyperSwiperPage() {
+  const { isPlaying, connect, resetGame, disconnectPriceFeed, toasts, removeToast } =
+    useTradingStore()
+
+  useEffect(() => {
+    sdk.actions.ready().catch(console.error)
+
+    connect()
+
+    return () => {
+      resetGame()
+      disconnectPriceFeed()
+    }
+  }, [connect, resetGame, disconnectPriceFeed])
+
+  return (
+    <div className="h-dvh w-screen bg-tron-black relative overflow-hidden">
+      <ToastNotifications toasts={toasts} onRemove={removeToast} />
+
+      <GameOverModal />
+
+      <SettlementFlash />
+
+      <RoundEndFlash />
+
+      {!isPlaying ? (
+        <MatchmakingScreen />
+      ) : (
+        <>
+          <GameCanvasBackground />
+
+          <GameHUD />
+
+          <GameCanvas scene="TradingScene" />
+
+          <PositionIndicator />
+        </>
+      )}
+    </div>
+  )
+}
